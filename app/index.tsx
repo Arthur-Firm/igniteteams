0b98c31e51d18f1134051df5673228a46db845b1
Arthur-Firm <arthurfirminomartins@gmail.com>
2025-05-29 15:47:31 -0300

    Configurações iniciais feitas
    Configurações para CSS in JS realizada, criada a tipagem e definição de cores e fontes

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { ActivityIndicator } from "react-native";
+import { ThemeProvider } from "styled-components";
+import { Groups } from "@screens/Groups";
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+import theme from "../src/theme"
+export default function Index() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups/> : <ActivityIndicator/>}
+    </ThemeProvider>
+  );
+}
