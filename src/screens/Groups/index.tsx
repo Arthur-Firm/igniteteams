56949ca0e5b8a23bd7705138584a61748981ed10
Arthur-Firm <arthurfirminomartins@gmail.com>
2025-05-31 20:39:36 -0300

    add Componentes GroupCard e Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from "@/src/components/Header";
 import { Container, Title } from "./styles";
+import { Highlight } from "@/src/components/Highlight";
+import { GroupCard } from "@/src/components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header/>
+      <Highlight title="Turmas" subtitle="Jogue com sua turma"/>
+
+      <GroupCard title="Galera do Ignite"/>
     </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
     flex: 1;
     align-items: center;
+    padding: 24px;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600}
 `
 
