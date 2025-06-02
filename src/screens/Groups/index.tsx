4c7399a7dc75ba0ca84e3cf2159dc6356e6714c3
Arthur-Firm <arthurfirminomartins@gmail.com>
2025-06-02 13:26:35 -0300

    add ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Header } from "@/src/components/Header";
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 import { Highlight } from "@/src/components/Highlight";
 import { GroupCard } from "@/src/components/GroupCard";
+import { ListEmpty } from "@/src/components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header/>
       <Highlight title="Turmas" subtitle="Jogue com sua turma"/>
-
-      <GroupCard title="Galera do Ignite"/>
+      <FlatList
+      style={{width: '100%'}}
+      data={groups}
+      keyExtractor={item => item}
+      contentContainerStyle={groups.length === 0 && {flex: 1}}
+      renderItem={({item}) => (
+        <GroupCard title={item}/>
+      )}
+      ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?"/>}
+      />
     </Container>
   );
 }
